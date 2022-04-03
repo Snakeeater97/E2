f327608b07481c85a025b9d727c162f00431e8a7
Snakeeater97 <14RO40550353@campus.economicas.uba.ar>
2022-04-03 10:30:35 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Por favor ingrese un texto: ");
+
+string texto = Console.ReadLine();
+
+Console.WriteLine($"Usted escribio: {texto}" );
